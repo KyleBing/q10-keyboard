c093186a8fefdf72418afc4f6dbf187521bf3ff1
KyleBing(MBP14) <kylebing@163.com>
2024-02-21 23:32:56 +0800

    显示点击的列表项内容

--- app/src/main/java/cn/kylebing/blackberry/demo_activity/MainActivity.java
+++ app/src/main/java/cn/kylebing/blackberry/demo_activity/MainActivity.java
@@ -3,29 +3,46 @@ package cn.kylebing.blackberry.demo_activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.device_list);
+        setContentView(R.layout.home);
 
 
         // LIST VIEW
-        final String[] list = {"BlackBerryQ10", "MacbookPro 14", "Windows(Win10)"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.device_list, list);
+        final String[] deviceStringArray = {"BlackBerryQ10", "MacbookPro 14", "Windows(Win10)"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_cell, R.id.list_cell, deviceStringArray);
 
-        ListView listView = (ListView) findViewById(R.id.listView);
+        ListView listView = findViewById(R.id.listView);
         listView.setAdapter(adapter);
+
+        // Handle Click
+        AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(
+                        view.getContext(),
+                        String.format("position: %d, id: %d\n你选择了 %s", position, id, deviceStringArray[position]),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        };
+
+        listView.setOnItemClickListener(messageClickedHandler);
 
 
 
 /*
         // button click event
-        final Button button = findViewById(R.id.button_1);
+        final Button button = findViewById(R.id.button_1);]
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 CharSequence text = "我的第一个 Toast!";
