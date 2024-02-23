4ba00749cb3df719349cec525663c72027193c52
KyleBing <kylebing@163.com>
2024-02-23 10:57:55 +0800

    添加蓝牙控制按钮

--- app/src/main/java/cn/kylebing/blackberry/q10_keyboard/MainActivity.kt
+++ app/src/main/java/cn/kylebing/blackberry/q10_keyboard/MainActivity.kt
@@ -5,6 +5,7 @@ import android.bluetooth.BluetoothManager
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -18,7 +19,8 @@ class MainActivity : AppCompatActivity() {
 
         // BLUETOOTH State
         val bluetoothAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)
-        val bluetoothLEAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
+        val bluetoothLEAvailable =
+            packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
 
         val textViewBluetooth = findViewById<TextView>(R.id.textViewBlueTooth)
         textViewBluetooth.text = "蓝牙：$bluetoothAvailable"
@@ -28,38 +30,90 @@ class MainActivity : AppCompatActivity() {
 
         refreshBlueToothInfo() // 刷新蓝牙信息
 
-        // button click
+        // bluetooth on
+        findViewById<Button>(R.id.button_turn_on_bluetooth)
+            .setOnClickListener {
+                openBluetooth()
+            }
+
+        // bluetooth on
         findViewById<Button>(R.id.button_refresh)
-            .setOnClickListener{
+            .setOnClickListener {
                 refreshBlueToothInfo()
+            }
+
+        // bluetooth found
+        findViewById<Button>(R.id.button_be_found)
+            .setOnClickListener {
+                setBluetoothCanBeFound()
             }
     }
 
-    private fun refreshBlueToothInfo(){
+    /**
+     * 设置蓝牙可被发现
+     */
+    private fun setBluetoothCanBeFound() {
+        val requestCode = 1;
+        val discoverableIntent: Intent =
+            Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+                putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 100)
+            }
+        startActivityForResult(discoverableIntent, requestCode)
+    }
+
+    /**
+     * 开启蓝牙
+     */
+    private fun openBluetooth() {
         // blue tooth
         val mBluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
-
         val textViewOther: TextView = findViewById<TextView>(R.id.textViewOther)
-        textViewOther.text = mBluetoothManager.toString()
-
-
         val mBtAdapter = mBluetoothManager.adapter
         if (mBtAdapter != null) {
-            textViewOther.text = "配对的蓝牙设备数量为 ${mBtAdapter.bondedDevices.size}"
-            if (!mBtAdapter.isEnabled){
+
+            if (!mBtAdapter.isEnabled) {
                 val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 val REQUEST_ENABLE_BT = 1 // 请求码，将会在结果中返回请求码
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
                 textViewOther.text = "蓝牙未开启，已请求开启"
+                object : CountDownTimer(3000,1000){
+                    override fun onTick(p0: Long) {
+                        textViewOther.text = "${p0/1000}s 后刷新蓝牙列表"
+                    }
+                    override fun onFinish() {
+                        refreshBlueToothInfo()
+                    }
+                }.start()
             } else {
+                textViewOther.text = "配对的蓝牙设备数量为 ${mBtAdapter.bondedDevices.size}"
                 var deviceSetString = ""
-                for (device in mBtAdapter.bondedDevices){
+                mBtAdapter.bondedDevices.forEach { device ->
                     deviceSetString = deviceSetString + device.toString() + ","
                 }
-                textViewOther.text = "配对的蓝牙设备数量为：${mBtAdapter.bondedDevices.size}个\n 设备信息为：$deviceSetString"
+                textViewOther.text =
+                    "配对的蓝牙设备数量为：${mBtAdapter.bondedDevices.size}个\n 设备信息为：$deviceSetString"
             }
-        } else {
-            textViewOther.text = "没有匹配的蓝牙"
+        }
+    }
+
+    /**
+     * 刷新蓝牙设备列表
+     * 只关注刷新列表，不管开没开启
+     */
+    private fun refreshBlueToothInfo() {
+        // blue tooth
+        val mBluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        val textViewOther: TextView = findViewById<TextView>(R.id.textViewOther)
+        val mBtAdapter = mBluetoothManager.adapter
+        if (mBtAdapter != null) {
+            if (mBtAdapter.isEnabled) {
+                var deviceSetString = ""
+                mBtAdapter.bondedDevices.forEach { device ->
+                    deviceSetString = deviceSetString + device.toString() + ","
+                }
+                textViewOther.text =
+                    "配对的蓝牙设备数量为：${mBtAdapter.bondedDevices.size}个\n 设备信息为：$deviceSetString"
+            }
         }
     }
 }
